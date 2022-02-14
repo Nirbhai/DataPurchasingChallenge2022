7a4affa439f6c3c6b177dc0245e90104c9c34f59
Nirbhai Singh <chittamor@gmail.com>
2022-02-14 17:55:53 +0530

    started building random-selection approach and moved the local repo to a new directory, hopefully nothing breaks

---
+++ randomSelection.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+
+
+
+Author	: Nirbhai Singh
+E-Mail	: chittamor@gmail.com
+
+
+*********************************
+
+Run below commands to verify you are in correct environment and branch:
+    !conda info
+    !git branch -a
+
+*********************************
+
+
+"""
+
+import pandas as pd
+import numpy as np
+
+classes = ['scratch_small',
+           'scratch_large',
+           'dent_small',
+           'dent_large']
+
+from typing import List
+
+def set_details(filename: str) -> (pd.DataFrame, List[int]):
+    """
+    
+
+    Parameters
+    ----------
+    filename : str
+        DESCRIPTION.
+
+    Returns
+    -------
+    (pd.DataFrame, List[int])
+        DESCRIPTION.
+
+    """
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
